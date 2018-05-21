c40119c063ac4a21cc846c8cf486c9e719c1b74b
Stratos Vetsos <vetsos.s@gmail.com>
2018-05-21 23:58:01 +0300

    remove unneeded state prop

--- src/components/App/partials/Header/partials/Searchable/index.js
+++ src/components/App/partials/Header/partials/Searchable/index.js
@@ -11,8 +11,7 @@ const mapStateToProps = ( state ) => {
 		queryString: state.search.queryString,
 		focused: state.focused,
 		ajax: state.ajax,
-		tags: state.tags,
-		id: state.id
+		tags: state.tags
 	}
 
 }
